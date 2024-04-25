21b01edf04de5e152d0112d2ffbbf49cbe04bc2e
Andrei Starichenko <centroid@mail.ru>
2024-04-25 15:35:37 +0300

    try

---
+++ test__1.py
@@ -0,0 +1,33 @@
+import os
+import pytest
+import pathlib
+import shutil
+from tempfile import TemporaryDirectory
+from typing import *
+from configparser import ConfigParser
+
+
+# =====================================================================================================================
+# KEEP FILES IN ROOT! OR IMPORT PRJ_MODULE WOULD FROM SYSTEM! NOT THIS SOURCE!!!
+from logger_aux import *
+
+
+# =====================================================================================================================
+class Test__888888888888:
+    @classmethod
+    def setup_class(cls):
+        pass
+
+    @classmethod
+    def teardown_class(cls):
+        pass
+
+    def setup_method(self, method):
+        self.VICTIM = type("VICTIM", (NEW_CLASS____,), {})
+
+    # -----------------------------------------------------------------------------------------------------------------
+    def test__ClassMethod_and_obj(self):
+        assert True
+
+
+# =====================================================================================================================
